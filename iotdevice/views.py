58b9195718adac24d9c10fab696c85b6dff64740
sina <eb.sina@gmail.com>
2019-01-31 18:24:48 +0330

    send-command-ok

--- iotdevice/views.py
+++ iotdevice/views.py
@@ -5,9 +5,11 @@ from asgiref.sync import async_to_sync
 
 
 def command(request, device_id):
-    channel_id = Device.objects.get(device_id=device_id).channel_id
-    channel_layer = get_channel_layer()
-    async_to_sync(channel_layer.send)(channel_id, {
-        "type": "command.message",
-        "command": "Hello there!",
-    })
+    if request.method == 'POST':
+        channel_id = Device.objects.get(device_id=device_id).channel_id
+        channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.send)(channel_id, {
+            "type": "command.message",
+            "command": request.POST['command'],
+        })
+    return render(request, 'command.html')
